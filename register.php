5b500df5952967c9b4ac51418fc6e9cfaf043589
margleb <margleb@yandex.ru>
2020-07-07 22:40:53 +0300

    Finishing touches - Finally

--- register.php
+++ register.php
@@ -114,6 +114,14 @@ if(isset($_POST['register_button'])) {
 		}
 
 		$query = mysqli_query($con, "INSERT INTO users VALUES ('', '$fname', '$lname', '$username', '$em', '$password', '$date', '$profile_pic', '0', '0', 'no',',')");
+
+		array_push($error_array, "<span style='color: #13C800;'>You're all set! Goahead and login!</span><br>");
+
+		//Clear session variables
+		$_SESSION['reg_fname'] = "";
+		$_SESSION['reg_lname'] = "";
+		$_SESSION['reg_email'] = "";
+		$_SESSION['reg_email2'] = "";
 		
 	}
 }
@@ -155,6 +163,9 @@ if(isset($_POST['register_button'])) {
 		else if(in_array("Your password must be between 5 and 30 characters<br>", $error_array)) echo "Your password must be between 5 and 30 characters<br>"; ?>
 
 		<input type="submit" name="register_button" value="Register">
+		<br>
+		<?php if(in_array("<span style='color: #13C800;'>You're all set! Goahead and login!</span><br>", $error_array)) echo "<span style='color: #13C800;'>You're all set! Goahead and login!</span><br>"; ?>
+
 	</form>
 </body>
 </html>
