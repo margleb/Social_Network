2b4a3c35fca43a56d3e8983f6c3675a7d58b22e9
Gleb <margleb@yandex.ru>
2020-07-07 10:05:53 +0300

    Getting values from the form

--- register.php
+++ register.php
@@ -1,3 +1,51 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "social");
+
+
+if(mysqli_connect_errno()) {
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+
+//Declaring variables to prevent errors
+$fname = ""; // First name
+$lname = ""; // Last name
+$em = ""; // email
+$em2 = ""; // email2
+$password = ""; // password
+$password2 = ""; // password 2
+$date = ""; //Sign up date
+$error_array = ""; //Holds error message
+
+if(isset($_POST['register_button'])) {
+
+	// Registration form values
+
+	// First name
+	$fname = stript_tags($_POST['reg_fname']);  // Удаляет теги HTML
+	$fname = str_replace(' ', '', $fname); // Удаляет пробелы
+	$fname = ucfirst(strtolower($fname)); // Первая заглавная буква
+	// Last name
+	$fname = stript_tags($_POST['reg_lname']);  // Удаляет теги HTML
+	$fname = str_replace(' ', '', $fname); // Удаляет пробелы
+	$fname = ucfirst(strtolower($fname)); // Первая заглавная буква
+	// Email
+	$em = stript_tags($_POST['reg_email']);  // Удаляет теги HTML
+	$em = str_replace(' ', '', $em); // Удаляет пробелы
+	$em = ucfirst(strtolower($em)); // Первая заглавная буква
+	// Email 2
+	$em2 = stript_tags($_POST['reg_email2']);  // Удаляет теги HTML
+	$em2 = str_replace(' ', '', $em2); // Удаляет пробелы
+	$em2 = ucfirst(strtolower($em2)); // Первая заглавная буква
+	// Password
+	$password = stript_tags($_POST['reg_password']);  // Удаляет теги HTML
+	$password2 = stript_tags($_POST['reg_password2']);  // Удаляет теги HTML
+	// Дата
+	$date = date("Y-m-d"); // Current date
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
