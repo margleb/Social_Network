6cdb0f2398638165cd6bb2034f95b128c1bdee7c
margleb <margleb@yandex.ru>
2020-07-09 08:37:01 +0300

    Navigation Bar Icons

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,8 @@
 <?php require 'config/config.php'; 
 if(isset($_SESSION['username'])) {
 	$userLoggedIn = $_SESSION['username'];
+	$user_detail_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+	$user = mysqli_fetch_array($user_detail_query);
 } else {
 	header("Location: register.php");
 } ?>
@@ -8,6 +10,26 @@ if(isset($_SESSION['username'])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Wekcome to Swirlfeed</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.bundle.js"></script>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+
+<div class="top_bar">
+	<div class="logo">
+		<a href="index.php">Swirlfeed!</a>
+	</div>
+	<nav>
+		<a href="#"><?php echo $user['first_name']; ?></i></a>
+		<a href="#"><i class="fa fa-home fa-lg"></i></a>
+		<a href="#"><i class="fa fa-envelope fa-lg"></i></a>
+		<a href="#"><i class="fa fa-bell-o fa-lg"></i></a>
+		<a href="#"><i class="fa fa-users fa-lg"></i></a>
+		<a href="#"><i class="fa fa-cog fa-lg"></i></a>
+	</nav>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include("includes/header.php");
-session_destroy();
+//session_destroy();
 ?>
 hello!
 </body>
