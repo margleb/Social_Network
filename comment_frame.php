8c1ad690dca7cb7ad4f8ba450724f691cc987376
margleb <margleb@yandex.ru>
2020-07-14 08:59:21 +0300

    Displaying the Comment Section

--- comment_frame.php
+++ comment_frame.php
@@ -39,9 +39,9 @@
 	$posted_to = $row['added_by'];
 	if(isset($_POST['postComment' . $post_id])) {
 		$post_body = $_POST['post_body'];
-		$post_body = mysqli_escape_string($_con, $post_body);
+		$post_body = mysqli_escape_string($con, $post_body);
 		$date_time_now = date("Y-m-d H:i:s");
-		$insert_post = mysqli_query($con, "INSERT INTO comments VALUES ('', '$post_body', '$userLoggedIn', '$posted_to', '$date_time_now', 'no', '$post_id'");
+		$insert_post = mysqli_query($con, "INSERT INTO comments VALUES ('', '$post_body', '$userLoggedIn', '$posted_to', '$date_time_now', 'no', '$post_id')");
 		echo "<p>Comment Posted!</p>";
 	}
 
--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -102,6 +102,21 @@ class Post {
 					$last_name = $user_row['last_name'];
 					$profile_pic = $user_row['profile_pic'];
 
+					?>
+
+					<script>
+					function toggle<?php echo $id; ?>() {
+						var element = document.getElementById("toggleComment<?php echo $id; ?>");
+						if(element.style.display == "block") {
+							element.style.display = "none";
+						} else {
+							element.style.display = "block";
+						}
+					}	
+					</script>
+
+					<?php
+
 					//Timeframe
 					$date_time_now = date('Y-m-d H:i:s');
 					$start_date = new DateTime($date_time); // Time Of Post
@@ -157,15 +172,20 @@ class Post {
 						}
 					}
 
-					$str .= "<div class='status_post'>
+					$str .= "<div class='status_post' onClick='javascript:toggle$id()'>
 								<div class='post_profile_pic'>
 									<img src='$profile_pic' width='50'>
 								</div>
 								<div class='posted_by' style='color:#ACACAC'>
 									<a href='$added_by'> $first_name $last_name </a> $user_to &nbsp&nbsp&nbsp;$time_message
 								</div>
-								<div id='post_body'>$body<br></div>
-							</div><hr>";
+								<div id='post_body'>$body<br>
+								</div>
+								<div class='post_comment' id='toggleComment$id' style='display:none;'>
+									<iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'></iframe>
+								</div>
+							</div>
+							<hr>";
 				}
 			} // End while loop
 
