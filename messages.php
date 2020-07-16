dbbc2e168c85d49e66380524251cc616f3e37901
margleb <margleb@yandex.ru>
2020-07-16 10:01:34 +0300

    Sending Messages

--- messages.php
+++ messages.php
@@ -14,6 +14,13 @@ else
 
 if($user_to != "new") {
 	$user_to_obj = new User($con, $user_to);
+	if(isset($_POST['post_message'])) {
+		if(isset($_POST['message_body'])) {
+			$body = mysqli_real_escape_string($con, $_POST['message_body']);
+			$date = date("Y-m-d H:i:s");
+			$message_obj->sendMessage($user_to, $body, $date);
+		}
+	}
 }
 
 ?>
