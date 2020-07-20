97f3a03721c8faa52cf8a233825605164b192edd
margleb <margleb@yandex.ru>
2020-07-20 09:48:43 +0300

    Profile Page Messages - Adding our Message System

--- profile.php
+++ profile.php
@@ -1,6 +1,8 @@
 <?php 
 include("includes/header.php");
 
+	$message_obj = new Message($con, $userLoggedIn);
+
 	if(isset($_GET['profile_username'])) {
 		$username = $_GET['profile_username'];
 		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
@@ -18,6 +20,22 @@ include("includes/header.php");
 	}
 	if(isset($_POST['respond_request'])) {
 		header("Location: request.php");
+	}
+
+
+	if(isset($_POST['post_message'])) {
+		if(isset($_POST['message_body'])) {
+			$body = mysqli_real_escape_string($con, $_POST['message_body']);
+			$date = date("Y-m-d H:i:s");
+			$message_obj->sendMessage($username, $body, $date);
+		}
+
+		$link = '#profileTabs a[href="#messages_div"]';
+		echo "<script>
+			$(function() {
+				$('".$link."').tab('show');
+			});
+		</script>";
 	}
 
 ?>
@@ -74,8 +92,44 @@ include("includes/header.php");
 	</div>
 
 	<div class="profile_main_column column">
-		<div class="posts_area"></div>
-		<img id="loading" src="assets/images/icons/loading.gif">
+
+		<ul class="nav nav-tabs" role="tablist" id="profileTabs">
+		  <li class="nav-item">
+		    <a class="nav-link active" href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-toggle="tab">Newsfeed</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#messages_div" aria-controls="messages_div" role="tab" data-toggle="tab">Messages</a>
+		  </li>
+		</ul>
+
+		<div class="tab-content">
+
+			<div role="tabpanel" class="tab-pane fade in active" id="newsfeed_div">
+				<div class="posts_area"></div>
+				<img id="loading" src="assets/images/icons/loading.gif">				
+			</div>
+			<div role="tabpanel" class="tab-pane fade" id="messages_div">
+			<?php 
+				echo "<h4>You and <a href='".$username."'>" . $profile_user_obj->getFirstAndLastName() . "</a></h4><hr><br>";
+				echo "<div class='loaded_messages' id='scroll_messages'>";
+				echo $message_obj->getMessages($username);
+				echo "</div>";
+			?>
+
+			<div class="message_post">
+				<form action="" method="POST">
+					<textarea name='message_body' id='message_textarea' placeholder='Write your message...'></textarea>
+					<input type='submit' name='post_message' class='info' id='message_submit' value='Send'>
+				</form>
+			</div>
+			<script type="text/javascript">
+				var div = document.getElementById("scroll_messages");
+				div.scrollTop = div.scrollHeight;
+			</script>	
+			</div>
+
+		</div>
+
 	</div>
 </div>
 
